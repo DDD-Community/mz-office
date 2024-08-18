77aa8fda0e8bf8275255fce5e473be280f56d7c1
nayoung <nayoung.tech@gmail.com>
2024-08-18 23:50:39 +0900

    feat: add urls for question

--- question/urls.py
+++ question/urls.py
@@ -1,20 +1,25 @@
 from django.urls import path
 from .views import (
     QuestionsListAPIView,
+    MyQuestionsListAPIView,
     QuestionAPIView,
     QuestionsAPIView,
     AnswerAPIView,
     Report,
     LikeView,
     Block,
+    AdminQuestionListAPIView
 )
 
 urlpatterns = [
     path('questions/', QuestionsListAPIView.as_view(), name='Questions'),
+    path('questions/me/', MyQuestionsListAPIView.as_view(), name='Questions'),
     path('question/', QuestionAPIView.as_view(), name='Question'),
     path('question/<int:pk>/', QuestionsAPIView.as_view(), name='Question'),
     path('questions/<int:pk>/vote', AnswerAPIView.as_view(), name='Vote'),
     path('questions/<int:pk>/report/', Report.as_view(), name='Report'),
     path('questions/<int:pk>/like/', LikeView.as_view(), name='Like'),
     path('questions/<int:pk>/block/', Block.as_view(), name='Block'),
+
+    path('back-office/questions/', AdminQuestionListAPIView.as_view(), name='')
 ]
