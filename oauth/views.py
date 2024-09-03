d84b78ed63a900e65f2ad950a4dfe32c15a892f6
integerJI <jjs9536@gmail.com>
2024-09-03 22:56:13 +0900

    Feat: 로그인 로그 제거

--- oauth/views.py
+++ oauth/views.py
@@ -29,7 +29,7 @@ def login_api(social_type: str, social_id: str, email: str=None, phone: str=None
     '''
     회원가입 및 로그인
     '''
-    logger.debug("1. login_api 호출됨 - social_type: %s, social_id: %s, email: %s", social_type, social_id, email)
+    
     login_view = LoginView()
     try:
         UserModel.objects.get(social_id=social_id)
@@ -37,10 +37,10 @@ def login_api(social_type: str, social_id: str, email: str=None, phone: str=None
             'social_id': social_id,
             'email': email,
         }
-        logger.debug("2. UserModel 조회 성공 - social_id: %s", social_id)
+        
         response = login_view.object(data=data)
     except UserModel.DoesNotExist:
-        logger.debug("3. UserModel 조회 실패 - 신규 사용자, social_id: %s", social_id)
+        
         data = {
             'social_type': social_type,
             'social_id': social_id,
@@ -48,10 +48,10 @@ def login_api(social_type: str, social_id: str, email: str=None, phone: str=None
         }
         user_view = UserView()
         login = user_view.get_or_create_user(data=data)
-        logger.debug("4. 신규 사용자 생성 결과 - status_code: %s", login.status_code)
+        
         response = login_view.object(data=data) if login.status_code == 201 else login
 
-    logger.debug("5. login_api 응답 - status_code: %s", response.status_code)
+    
     return response
 
 
@@ -68,7 +68,7 @@ class KakaoLoginView(APIView):
         redirect_uri = KAKAO.RECIRECT_URI
         uri = f"{KAKAO.LOGIN_URL}?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
 
-        logger.debug("6. KakaoLoginView GET 호출 - 리다이렉트 URI: %s", uri)
+        
         res = redirect(uri)
         return res
 
@@ -78,13 +78,13 @@ class KakaoCallbackView(APIView):
 
     @swagger_auto_schema(query_serializer=CallbackUserInfoSerializer)
     def get(self, request):
-        logger.debug("7. KakaoCallbackView GET 호출 - request query params: %s", request.query_params)
+        
         data = request.query_params
 
         # iOS에서 전달된 access_token 확인
         access_token = data.get('access_token')
         code = data.get('code')
-        logger.debug("8. KakaoCallbackView GET - code: %s, access_token: %s", code, access_token)
+        
 
         expires_in = 0
         refresh_token_expires_in = 0
@@ -101,9 +101,9 @@ class KakaoCallbackView(APIView):
             token_headers = {
                 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
             }
-            logger.debug("10. Kakao access_token 요청 - request_data: %s", request_data)
+            
             token_res = requests.post(KAKAO.TOKEN_URL, data=request_data, headers=token_headers)
-            logger.debug("11. Kakao access_token 응답 - status_code: %s, response: %s", token_res.status_code, token_res.text)
+            
 
             token_json = token_res.json()
             access_token = token_json.get('access_token')
@@ -111,13 +111,13 @@ class KakaoCallbackView(APIView):
             refresh_token_expires_in = token_json.get('refresh_token_expires_in')
 
             if not access_token:
-                logger.debug("12. Kakao access_token 응답 실패 - access_token 없음")
+                
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
             access_token = f"Bearer {access_token}"  # 'Bearer ' 마지막 띄어쓰기 필수
 
         elif not access_token:
-            logger.debug("9. KakaoCallbackView GET - code와 access_token 없음")
+            
             return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
             access_token = f"Bearer {access_token}"  # iOS에서 전달된 access_token 사용
@@ -127,9 +127,9 @@ class KakaoCallbackView(APIView):
             "Authorization": access_token,
             "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
         }
-        logger.debug("13. Kakao 회원정보 요청 - access_token: %s", access_token)
+        
         user_info_res = requests.get(KAKAO.PROFILE_URL, headers=auth_headers)
-        logger.debug("14. Kakao 회원정보 응답 - status_code: %s, response: %s", user_info_res.status_code, user_info_res.text)
+        
 
         user_info_json = user_info_res.json()
 
@@ -138,14 +138,14 @@ class KakaoCallbackView(APIView):
 
         kakao_account = user_info_json.get('kakao_account')
         if not kakao_account:
-            logger.debug("15. Kakao 회원정보 응답 실패 - kakao_account 없음")
+            
             return Response(status=status.HTTP_400_BAD_REQUEST)
         user_email = kakao_account.get('email')
 
         # 회원가입 및 로그인
-        logger.debug("16. login_api 호출 전 - social_type: %s, social_id: %s, email: %s", social_type, social_id, user_email)
+        
         res = login_api(social_type=social_type, social_id=social_id, email=user_email)
-        logger.debug("17. login_api 응답 - status_code: %s", res.status_code)
+        
 
         # 만약 `res`가 Response 객체라면, 데이터에 토큰 정보를 추가합니다.
         if isinstance(res, Response):
@@ -161,9 +161,9 @@ class KakaoCallbackView(APIView):
             res.data['data']['is_expires'] = is_expires
             res.data['data']['is_refresh_token_expires'] = is_refresh_token_expires
 
-        logger.debug("======================================")
-        logger.debug("Final. return res: %s", res)
-        logger.debug("======================================")
+        
+        
+        
 
         return res
 
@@ -248,7 +248,7 @@ class AppleLoginView(APIView):
         redirect_uri = APPLE.REDIRECT_URI
         uri = f"{APPLE.AUTH_URL}?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
 
-        logger.debug("1. AppleLoginView GET 호출 - 리다이렉트 URI: %s", uri)
+        
         res = redirect(uri)
         return res
 
@@ -260,7 +260,7 @@ class AppleCallbackView(APIView):
         '''
         CLIENT_SECRET 생성
         '''
-        logger.debug("2. AppleCallbackView CLIENT_SECRET 생성 시작")
+        
 
         headers = {
             'alg': 'ES256',
@@ -282,7 +282,7 @@ class AppleCallbackView(APIView):
             headers=headers
         )
 
-        logger.debug("3. CLIENT_SECRET 생성 완료")
+        
         return client_secret
 
     @swagger_auto_schema(query_serializer=CallbackAppleInfoSerializer)
@@ -292,21 +292,21 @@ class AppleCallbackView(APIView):
 
         ---
         '''
-        logger.debug("4. AppleCallbackView GET 호출 - request query params: %s", request.query_params)
+        
         data = request.query_params
 
         access_token = data.get('access_token')
         code = data.get('code')
-        logger.debug("5. AppleCallbackView GET - code: %s, access_token: %s", code, access_token)
+        
 
         # code와 access_token이 모두 없으면 에러 반환
         if not code and not access_token:
-            logger.debug("6. AppleCallbackView GET - code와 access_token 없음")
+            
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         if code:
             # code가 있을 때: Apple 서버에서 access_token 발급 요청
-            logger.debug("7. AppleCallbackView GET - code 사용하여 access_token 발급 요청")
+            
 
             client_secret = self.get_key_and_secret()
 
@@ -321,9 +321,9 @@ class AppleCallbackView(APIView):
                 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
             }
 
-            logger.debug("8. Apple access_token 요청 - request_data: %s", request_data)
+            
             token_res = requests.post(APPLE.TOKEN_URL, data=request_data, headers=token_headers)
-            logger.debug("9. Apple access_token 응답 - status_code: %s, response: %s", token_res.status_code, token_res.text)
+            
 
             token_json = token_res.json()
             access_token = token_json.get('access_token')
@@ -332,23 +332,23 @@ class AppleCallbackView(APIView):
             refresh_token_expires_in = token_json.get('refresh_token_expires_in', 0)
 
             if not id_token:
-                logger.debug("10. Apple access_token 응답 실패 - id_token 없음")
+                
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
         else:
             # access_token이 있을 때: 클라이언트에서 전달된 access_token 사용
-            logger.debug("7. AppleCallbackView GET - 클라이언트에서 전달된 access_token 사용")
+            
             id_token = access_token  # access_token이 곧 id_token 역할을 함
             expires_in = 0  # 만료 시간 정보가 없을 수 있으므로 0으로 설정
             refresh_token_expires_in = 0
 
         # id_token 디코딩 및 사용자 정보 추출
-        logger.debug("11. Apple id_token 디코딩 시작")
+        
         token_decode = jwt.decode(id_token, '', options={"verify_signature": False})
-        logger.debug("12. Apple id_token 디코딩 완료 - token_decode: %s", token_decode)
+        
 
         if (not token_decode.get('sub')) or (not token_decode.get('email')) or (not token_decode.get('email_verified')):
-            logger.debug("13. Apple id_token 유효하지 않음 - sub: %s, email: %s, email_verified: %s",
+            
                          token_decode.get('sub'), token_decode.get('email'), token_decode.get('email_verified'))
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -357,9 +357,9 @@ class AppleCallbackView(APIView):
         user_email = token_decode['email']
 
         # 회원가입 및 로그인
-        logger.debug("14. login_api 호출 전 - social_type: %s, social_id: %s, email: %s", social_type, social_id, user_email)
+        
         res = login_api(social_type=social_type, social_id=social_id, email=user_email)
-        logger.debug("15. login_api 응답 - status_code: %s", res.status_code)
+        
 
         # 토큰 정보 추가
         if isinstance(res, Response):
@@ -375,9 +375,9 @@ class AppleCallbackView(APIView):
             res.data['data']['is_expires'] = is_expires
             res.data['data']['is_refresh_token_expires'] = is_refresh_token_expires
 
-        logger.debug("======================================")
-        logger.debug("Final. return res: %s", res)
-        logger.debug("======================================")
+        
+        
+        
 
         return res
 
